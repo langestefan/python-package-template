de1f674874abb01b85cffc105064d7439637fab6
Stefan <langestefan@msn.com>
2024-01-27 19:26:31 +0000

    Unbork git

--- tests/unit/mymodule/test_mymodule.py
+++ tests/unit/mymodule/test_mymodule.py
@@ -8,10 +8,10 @@ class TestMyModule:
     """Test my module."""
 
     @pytest.fixture
-    def some_fixture(self):
+    def some_fixture(self) -> bool:
         """Fixture."""
         return True
 
-    def test_my_module(self, some_fixture):
+    def test_my_module(self, *, some_fixture: bool) -> None:
         """Test my module."""
         assert some_fixture
